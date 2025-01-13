0bc41cb46f03b307aa7463bd7289b82e2533fcce
salihkadir <139787733+salihkadir@users.noreply.github.com>
2025-01-13 17:39:46 +0200

    languageVersion fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -158,8 +158,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(17)
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 application {
